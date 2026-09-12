246ae9d2ce3f1f2dd2b23c25ada4434e5bde2a88
William Rogers <williamcjrogers@gmail.com>
2026-09-12 04:28:32 +0100

    chore: remove unused declarations causing build warnings

--- src/components/ui/CountUp.tsx
+++ src/components/ui/CountUp.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { motion, useInView } from "framer-motion";
+import { useInView } from "framer-motion";
 
 interface CountUpProps {
   value: string;
